bcfb84350eab8ee93191addf3eec32065099f8ea
yakov.burtsev <yakovburtsev@gmail.com>
2019-02-04 21:56:07 +0300

    remove unused @EnableWebSecurity

--- src/main/java/readinglist/config/SecurityConfig.java
+++ src/main/java/readinglist/config/SecurityConfig.java
@@ -4,14 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import readinglist.repository.ReaderRepository;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -21,14 +19,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/").access("hasRole('READER')")
-                    .antMatchers("/**").permitAll()
+                .antMatchers("/").access("hasRole('READER')")
+                .antMatchers("/**").permitAll()
 
                 .and()
 
                 .formLogin()
-                    .loginPage("/login")
-                    .failureUrl("/login?error=true");
+                .loginPage("/login")
+                .failureUrl("/login?error=true");
     }
 
     @Override
