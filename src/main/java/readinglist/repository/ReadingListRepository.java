23bbd11a863274472032a587c999eff7b46a7b94
yakov.burtsev <yakovburtsev@gmail.com>
2019-02-05 21:15:45 +0300

    optimize imports

--- src/main/java/readinglist/repository/ReadingListRepository.java
+++ src/main/java/readinglist/repository/ReadingListRepository.java
@@ -1,7 +1,7 @@
 package readinglist.repository;
 
-import readinglist.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import readinglist.model.Book;
 import readinglist.model.Reader;
 
 import java.util.List;
