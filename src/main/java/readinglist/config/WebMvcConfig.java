77d7458adf52a3b947efadd7a64ae6d53ebe2ef1
yakov.burtsev <yakovburtsev@gmail.com>
2019-03-12 01:00:16 +0300

    use @AuthenticationPrincipal

--- src/main/java/readinglist/config/WebMvcConfig.java
+++ src/main/java/readinglist/config/WebMvcConfig.java
@@ -1,17 +1,8 @@
 package readinglist.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.MethodParameter;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.support.WebDataBinderFactory;
-import org.springframework.web.context.request.NativeWebRequest;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.method.support.ModelAndViewContainer;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import readinglist.model.Reader;
-
-import java.util.List;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -19,29 +10,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
-    }
-
-    @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-
-        argumentResolvers.add(new HandlerMethodArgumentResolver() {
-
-            @Override
-            public boolean supportsParameter(MethodParameter parameter) {
-                return Reader.class.isAssignableFrom(parameter.getParameterType());
-            }
-
-            @Override
-            public Object resolveArgument(MethodParameter parameter,
-                                          ModelAndViewContainer mavContainer,
-                                          NativeWebRequest webRequest,
-                                          WebDataBinderFactory binderFactory) {
-
-                Authentication auth = (Authentication) webRequest.getUserPrincipal();
-                return auth != null && auth.getPrincipal() instanceof Reader ? auth.getPrincipal() : null;
-
-            }
-        });
     }
 
 }
--- src/main/java/readinglist/controller/ReadingListController.java
+++ src/main/java/readinglist/controller/ReadingListController.java
@@ -2,6 +2,7 @@ package readinglist.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -29,7 +30,7 @@ public class ReadingListController {
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public String readersBooks(Reader reader, Model model) {
+    public String readersBooks(@AuthenticationPrincipal Reader reader, Model model) {
         List<Book> readingList = readingListRepository.findByReader(reader);
         if (readingList != null) {
             model.addAttribute("books", readingList);
@@ -40,7 +41,7 @@ public class ReadingListController {
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public String addToReadingList(Reader reader, Book book) {
+    public String addToReadingList(@AuthenticationPrincipal Reader reader, Book book) {
         book.setReader(reader);
         readingListRepository.save(book);
         return "redirect:/";
